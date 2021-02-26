7f861d9ee3cd0fc1a9697cb04d81be69be245053
Nicolas Sterchele <sterchelen@gmail.com>
2021-02-26 21:28:22 +0100

    add basic args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "ariane", about = "know your path")]
+struct Opt {
+    #[structopt(short, long)]
+    debug: bool,
+    #[structopt(short, long)]
+    max_hop: u8,
+    #[structopt(short, long)]
+    tries: u8,
+    #[structopt(short, long)]
+    port: u32,
+    #[structopt(short, long)]
+    icmp: bool,
+}
+
 fn main() {
     println!("Hello, world!");
 }
